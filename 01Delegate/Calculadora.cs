3cad8f49e970106769e7e5caa59091cfc674337e
Jessé Caldas <jessencaldas@hotmail.com>
2023-07-06 16:34:44 -0300

    add 01

---
+++ 01Delegate/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿namespace _01Delegates;
+
+public class Calculadora
+{
+    public static float Somar (float n1, float n2)
+    {
+        return n1 + n2;
+    }
+    
+    public static float Multiplicar (float n1, float n2)
+    {
+        return n1 * n2;
+    }
+
+    public static float Subtrair(float n1, float n2)
+    {
+        return n1 - n2;
+    }
+
+    public static float Dividir(float n1, float n2)
+    {
+        return n1 / n2;
+    }
+}
--- 01Delegate/Program.cs
+++ 01Delegate/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _01Delegates;
+
+Console.WriteLine("Delegates");
+
+Console.WriteLine("\nCalculo utilizando classe converncional");
+var resultado = Calculadora.Somar(10, 40);
+Console.WriteLine($"10 + 40 = {resultado}");
+
+Console.WriteLine("\nCalculos utilizarndo Delegates");
+
+//Declaração de Delegate
+
+//Forma 1:
+DelegateCalculadora somar = new DelegateCalculadora(Calculadora.Somar);
+var resultado1 = somar.Invoke(10, 40);
+Console.WriteLine($"\n10 + 40 = {resultado1}");
+
+//Forma 2:
+DelegateCalculadora multiplicar = Calculadora.Multiplicar;
+var resultado2 = multiplicar.Invoke(10, 40);
+Console.WriteLine($"\n10 * 40 = {resultado2}");
+
+//Forma 3:
+DelegateCalculadora subtratir = (float x, float y) => Calculadora.Subtrair(x, y);
+var resultado3 = subtratir.Invoke(40, 10);
+Console.WriteLine($"\n40 - 10 = {resultado3}");
+
+public delegate float DelegateCalculadora (float x, float y); //Criação de Delegate
