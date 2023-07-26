591d8cc911d0604e173fe31a608f9d8a625c7680
Jessé Caldas <jessencaldas@hotmail.com>
2023-07-26 16:35:11 -0300

    add exercicio 05 e 06

--- Exercicio05/Program.cs
+++ Exercicio05/Program.cs
@@ -4,3 +4,15 @@ Crie um programa onde um método que receba dois números inteiros e retorne o r
 de adição ou subtração desses números, a ser definida em tempo de execução por um delegate Func<int, int,
 int>.
  */
+
+Console.WriteLine("Digite um número: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nDigite outro número: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Func<int, int, int> operacao = (x, y) =>  x + y;
+Console.WriteLine($"\nA soma de {n1} e {n2} é {operacao(n1, n2)}");
+
+operacao = (x, y) =>  x - y;
+Console.WriteLine($"\nA subtracao de {n1} e {n2} é {operacao(n1, n2)}");
