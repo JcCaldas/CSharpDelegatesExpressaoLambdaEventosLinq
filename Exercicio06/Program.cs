0ba7943e897359301d9cbdd6bf8856c3104839bf
Jessé Caldas <jessencaldas@hotmail.com>
2023-07-27 15:46:47 -0300

    add Exercicio 06 e 07

--- Exercicio06/Program.cs
+++ Exercicio06/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Exercício 06\n");
+/*
+Suponha que você possui uma lista de planetas com informações sobre o nome, diâmetro e massa de cada
+planeta.
+Crie um programa e implemente um método que recebe essa lista e um delegate Predicate<Planeta> para
+filtrar os planetas de acordo com um critério específico.
+*/
+
+List<Planeta> planetas = new List<Planeta>
+{
+    new Planeta("Mercúrio", 4879, 3.301e23),
+    new Planeta("Vênus", 12104, 4.867e24),
+    new Planeta("Terra", 12756, 5.972e24),
+    new Planeta("Marte", 6792, 6.39e23),
+    new Planeta("Júpiter", 142984, 1.898e27),
+    new Planeta("Saturno", 120536, 5.683e26),
+    new Planeta("Urano", 51118, 8.681e25),
+    new Planeta("Netuno", 49528, 1.024e26)
+};
+
+//Filtrar os planetas com diâmetro maior que 10000 km
+Predicate<Planeta> filtrarMassa = massa => massa.Diametro > 10000;
+
+List<Planeta> planetasMassaGrande = Filtrar(planetas, filtrarMassa);
+
+Console.WriteLine("Planetas com diâmetro maior que 10000 km:");
+foreach (Planeta planeta in planetasMassaGrande)
+{
+    Console.WriteLine(planeta.Nome);
+}
+
+Console.ReadKey();
+
+
+//Método para adicionar planetas filtrados em nova lista
+static List<Planeta> Filtrar (List<Planeta> lista, Predicate<Planeta> filtro)
+{
+    List<Planeta> resultado = new List<Planeta>();
+    foreach (Planeta planeta in lista)
+    {
+        if (filtro(planeta))
+        {
+            resultado.Add(planeta);
+        }
+    }
+    return resultado;
+}
+
+
+class Planeta
+{
+    public string Nome { get; set; }
+    public double Diametro { get; set; }
+    public double Massa { get; set; }
+    public Planeta(string nome, double diametro, double massa)
+    {
+        Nome = nome;
+        Diametro = diametro;
+        Massa = massa;
+    }
+}
---
+++ Exercicio07/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Exercício 07\n");
+/*Crie um método de extensão para a classe List<int> na linguagem C# que, dado uma lista de inteiros,
+retorne a soma de todos os elementos da lista que são ímpares.
+Em seguida, crie um programa que instancie uma lista de inteiros com alguns números ímpares e alguns
+números pares e use o método de extensão criado para obter a soma dos números ímpares da lista.*/
+
+List<int> lista = new List<int>()
+{
+    1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
+};
+
+Console.WriteLine($"A soma dos impares é {lista.SomaImpares()}");
+
+Console.ReadKey();
+
+public static class ListExtensions
+{
+    public static int SomaImpares(this List<int> lista)
+    {
+        int somaImpar = 0;
+        for (int i = 0; i < lista.Count; i++)
+        {
+            if (lista[i] % 2 == 0)
+                continue;
+            else
+                somaImpar += lista[i];
+        }
+        return somaImpar;
+    }
+}
